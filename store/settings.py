f46b4792e140d47f27ea76445f28668113b2b7cc
TechStuffBoy <nandhu03.m@gmail.com>
2022-07-12 08:00:06 +0530

    Add 404 error page.

--- store/settings.py
+++ store/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8n7s1!x=7jywdgq+(3n+7n6e+m&vs%fcv8wlwwcs^z5#30p0$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
